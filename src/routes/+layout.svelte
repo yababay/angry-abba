6856e72292f7f924a631939b686c2fa498dc3624
mabel <bigbelk@live.ru>
2023-11-14 00:24:55 +0300

    koteg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
     import '../styles/index.scss'
     import { Head, Footer } from '@yababay67/svelte-components'
-    import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte'
     import { author, description, title, subtitle, keywords } from '../SEO.json'
     const seo = { author, description, title, subtitle, keywords }
 </script>
-
-<ThemeSwitcher label="Уберите этот ужас!" />
 
 <Head {seo} />
 

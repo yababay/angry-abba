9bfa59ff51bc840329f479511557f4421895010b
mabel <bigbelk@live.ru>
2024-02-04 15:00:23 +0300

    Updates

---
+++ _app/immutable/chunks/paths.f028f8e4.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1sg2mut)==null?void 0:s.base)??"/angry-abba";var a;const e=((a=globalThis.__sveltekit_1sg2mut)==null?void 0:a.assets)??t;export{e as a,t as b};
